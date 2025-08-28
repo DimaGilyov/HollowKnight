5e19cb824b8a783ff1a8f20ee64d0ff2fda875e0
DimaG <dimagilev91@gmail.com>
2025-08-28 10:50:32 +0500

    dash

--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -24,13 +24,27 @@ var wall_y_forse: float = wall_y_forse_settings
 @onready var right_ray: RayCast2D = $raycast/right_ray
 var is_wall_jumping: bool = false 
 
+@export_category("Dash variables")
+@export var dash_speed: float = 400.0
+@export var facing_right: bool = true
+@export var dash_gravity: float = 0
+@export var dash_number = 1
+var dash_key_pressed: int = 0
+var is_dashing: bool = false
+var dash_timer: Timer
+
 func horizontal_movement() -> void:
-	if not is_wall_jumping:		
+	if not is_wall_jumping and not is_dashing:		
 		var direction: float = Input.get_axis("ui_left", "ui_right")
 		if direction:
 			velocity.x = direction * move_speed
 		else:
 			velocity.x = move_toward(velocity.x, 0, move_speed * deceleration)
+			
+	if Input.is_action_just_pressed("ui_dash") and dash_key_pressed == 0 and dash_number > 0:
+		dash_number -= 1
+		dash_key_pressed = 1
+		dash()
 		
 func set_animation() -> void:
 	if velocity.y < 0:
@@ -49,13 +63,18 @@ func flip() -> void:
 	if velocity.x > 0.0:
 		scale.x = scale.y * 1
 		wall_x_forse = wall_x_forse_settings
+		facing_right = true
 	if velocity.x < 0.0:
 		scale.x = scale.y * -1
 		wall_x_forse = -wall_x_forse_settings
+		facing_right = false
 
 func vertical_movement(delta: float) -> void:
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if not is_dashing:
+			velocity.y += gravity * delta
+		else:
+			velocity.y = dash_gravity
 		
 	jump_logic()
 	wall_logic()
@@ -63,6 +82,7 @@ func vertical_movement(delta: float) -> void:
  
 func jump_logic() -> void:
 	if is_on_floor():
+		dash_number = 1
 		jump_amount = 2
 		if Input.is_action_just_pressed("ui_accept"):
 			jump_amount -= 1			
@@ -94,6 +114,28 @@ func wall_jumping():
 	await  get_tree().create_timer(0.12).timeout		
 	is_wall_jumping = false
 		
+func dash():
+	if dash_key_pressed == 1:
+		is_dashing = true
+	else:
+		is_dashing = false
+		
+	if facing_right:
+		velocity.x = dash_speed
+		dash_started()
+	else:
+		velocity.x = -dash_speed
+		dash_started()
+
+func dash_started():
+	if is_dashing:
+		dash_key_pressed = 1
+		await  get_tree().create_timer(0.2).timeout
+		is_dashing = false
+		dash_key_pressed = 0
+	else:
+		return		
+
 func _physics_process(delta: float) -> void:
 	#print("fps=", Engine.get_frames_per_second())
 	horizontal_movement() 
